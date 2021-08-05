c2c94b6447d4aa01c1cb7be0510c088a5102e09f
rusairdrop <rusairdrop@gmail.com>
2021-08-05 16:45:51 +0300

    реализовал удаление задач со страницы

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let todoInput = document.querySelector('.todo_text');
 let todoBtn = document.querySelector('.todo_btn');
 let todo = document.querySelector('.todo');
-
 let todoList = [];
 
 todoBtn.addEventListener('click', function () {
@@ -11,23 +10,39 @@ todoBtn.addEventListener('click', function () {
     todo: todoInput.value,
     checked: false,
   }
+  
   todoList.push(newTodo);
   
-  displayTodoList()
+  displayTodoList();
   
   todoInput.value = '';
 });
 
 function displayTodoList() {
   let displayTodo = '';
-  todoList.map(function (item, index){
+  if (todoList.length === 0) todo.innerHTML = '';
+  todoList.map(function (item, index) {
     displayTodo += `
-      <li>
-        <input type="checkbox" id="item_${index}">
+      <li class="todo_item" data-attr="${index}">
+        <input type="checkbox" id="item_${index}" ${item.checked ? 'checked' : ''}>
         <label for="item_${index}">${item.todo}</label>
+        <img class="btn_close" src="./img/close.svg" alt="" data-attr="${index}">
       </li>
     `;
     todo.innerHTML = displayTodo;
-    console.log(displayTodo);
   });
 }
+
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('btn_close')) {
+    let btnAttr = event.target.getAttribute('data-attr');
+    
+    todoList.forEach(function (item, index) {
+      
+      if (index === +btnAttr) {
+        todoList.splice(index, 1);
+      }
+      displayTodoList();
+    })
+  }
+});
