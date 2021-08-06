4beb4aaaf1b75392557d0679ec3978d5f9535c65
rusairdrop <rusairdrop@gmail.com>
2021-08-06 17:20:12 +0300

    - разделил текущие и выполненные таски/задачи на два объекта, изменена логика их размещения на странице - дописаны и исправлены стили

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let todoInput = document.querySelector('.todo_text');
 let todoBtn = document.querySelector('.todo_btn');
 let todo = document.querySelector('.todo');
+let completed = document.querySelector('.completed');
 let todoList = [];
+let completedList = [];
 
 if (localStorage.getItem('todo')) {
   todoList = JSON.parse(localStorage.getItem('todo'));
   displayTodoList();
+}
+if (localStorage.getItem('completed')) {
+  completedList = JSON.parse(localStorage.getItem('completed'));
+  displayCompletedList();
 }
 
 todoBtn.addEventListener('click', function () {
@@ -30,15 +36,32 @@ function displayTodoList() {
   let displayTodo = '';
   todoList.map(function (item, index) {
     displayTodo += `
-      <li class="todo_item" data-attr="${index}">
-        <input type="checkbox" id="${index}" ${item.checked ? 'checked' : ''}>
-        <label for="${index}" style="${item.checked ? 'text-decoration: line-through' : ''}" >${item.todo}</label>
-        <img class="btn_close" src="./img/close.svg" alt="" data-attr="${index}">
+      <li class="todo_item" data-attr="todo_${index}">
+        <input type="checkbox" data-attr="${index}" id="todo_${index}" ${item.checked ? 'checked' : ''}>
+        <label for="todo_${index}" style="${item.checked ? 'text-decoration: line-through' : ''}" >${item.todo}</label>
+        <img class="btn_close" src="./img/close.svg" alt="" data-attr="todo_${index}">
       </li>
       `;
     
     todo.innerHTML = displayTodo;
   });
+}
+
+function displayCompletedList() {
+  if (completedList.length === 0) completed.innerHTML = '';
+  
+  let displayCompleted = '';
+  completedList.map(function (item, index) {
+    displayCompleted += `
+      <li class="todo_item" data-attr="completed_${index}">
+        <input type="checkbox" data-attr="${index}" id="completed_${index}" ${item.checked ? 'checked' : ''}>
+        <label for="completed_${index}" style="${item.checked ? 'text-decoration: line-through' : ''}" >${item.todo}</label>
+        <img class="btn_close" src="./img/close.svg" alt="" data-attr="completed_${index}">
+      </li>
+      `;
+    completed.innerHTML = displayCompleted;
+  });
+  
 }
 
 document.addEventListener('click', function (event) {
@@ -47,22 +70,33 @@ document.addEventListener('click', function (event) {
     
     todoList.forEach(function (item, index) {
       
-      if (index === +btnAttr) {
+      if ('todo_' + index === btnAttr) {
         todoList.splice(index, 1);
       }
       displayTodoList();
       localStorage.setItem('todo', JSON.stringify(todoList));
+    });
+    
+    completedList.forEach(function (item, index) {
+      
+      if ('completed_' + index === btnAttr) {
+        completedList.splice(index, 1);
+      }
+      displayCompletedList();
+      localStorage.setItem('completed', JSON.stringify(completedList));
     });
   }
 });
 
 todo.addEventListener('change', function (event) {
   let idInput = event.target.getAttribute('id');
-  let forLabel = todo.querySelector('[for="' + idInput + '"]');
-  let valueLabel = forLabel.innerHTML;
+  let forLabelTodo = todo.querySelector('[for="' + idInput + '"]');
+  let valueLabelTodo = forLabelTodo.innerHTML;
+  let DataAtr = event.target.getAttribute('data-attr');
   
   todoList.forEach(function (item) {
-    if (item.todo === valueLabel) {
+    
+    if (item.todo === valueLabelTodo) {
       item.checked = !item.checked
     }
     
@@ -70,12 +104,39 @@ todo.addEventListener('change', function (event) {
   });
   
   if (event.target.checked) {
-    forLabel.style.textDecoration = 'line-through'
-    todoList.push(todoList.splice(idInput,1)[0]);
-  } else {
-    forLabel.style.textDecoration = ''
-    todoList.unshift(todoList.splice(idInput, 1)[0]);
+    forLabelTodo.style.textDecoration = 'line-through'
+    completedList.push(todoList.splice(DataAtr, 1)[0]);
   }
+  
   displayTodoList();
+  displayCompletedList();
   localStorage.setItem('todo', JSON.stringify(todoList));
+  localStorage.setItem('completed', JSON.stringify(completedList));
+});
+
+completed.addEventListener('change', function (event) {
+  
+  let idCompleted = event.target.getAttribute('id');
+  let forLabelCompleted = completed.querySelector('[for="' + idCompleted + '"]');
+  let valueLabelCompleted = forLabelCompleted.innerHTML;
+  let DataAtr = event.target.getAttribute('data-attr');
+  
+  completedList.forEach(function (item) {
+    if (item.todo === valueLabelCompleted) {
+      item.checked = !item.checked
+    }
+    
+    localStorage.setItem('completed', JSON.stringify(completedList));
+  });
+  
+  if (!event.target.checked) {
+    forLabelCompleted.style.textDecoration = ''
+    todoList.push(completedList.splice(DataAtr, 1)[0]);
+    forLabelCompleted.style.textDecoration = 'line-through'
+  }
+  
+  localStorage.setItem('todo', JSON.stringify(todoList));
+  localStorage.setItem('completed', JSON.stringify(completedList));
+  displayTodoList();
+  displayCompletedList();
 });
